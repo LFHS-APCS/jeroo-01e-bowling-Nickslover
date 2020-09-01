9a2622efd52a62d7d78c9dc93e3addf14dff343e
Nicholas Slover <2230018@lfschools.net>
2020-09-01 01:21:46 +0000

    I set up the bowling while using methods to make the code more readable

--- Jeroo.java
+++ Jeroo.java
@@ -5,7 +5,71 @@
 public class Jeroo extends JerooBase {
 
     // Put your own methods here    
-    
+public void hopPlanteother(){
+hop();
+hopPlant();  
+}
+
+public void hopPlant(){
+  hop();
+  plant();  
+}
+
+public void hopTurnR(){
+  hop();
+  turn(RIGHT);  
+}
+
+public void hopTurnL(){
+  hop();
+  turn(LEFT);
+}
+
+public void hopPlanteother3(){
+  hopPlanteother();
+  hopPlanteother();
+  hopPlanteother();
+}
+
+public void hopPlanteother2(){
+   hopPlanteother();
+   hopPlanteother();
+}
+
+public void line1(){
+  hopTurnR();
+  hopPlant();
+  turn(LEFT);
+  hopPlanteother3();
+  turn(RIGHT);
+}
+
+public void line2(){
+  hopTurnR();
+  hopPlant();
+  hopPlanteother2();
+  turn(LEFT);
+}
+
+public void line3(){
+  hopTurnL();
+  hopPlant();
+  hopPlanteother();
+  turn(RIGHT);
+}
+
+public void line4(){
+  hopTurnR();
+  hopPlant();
+  hop(3);
+}
+
+public void setupBowling(){
+  line1();
+  line2();
+  line3();
+  line4(); 
+}
     
 
 
